43a561a5b5a439190f5a5174cbf416cfdc37bab2
Rahma Binth Mohammad <rahmabinth@gmail.com>
2024-03-26 20:24:58 -0400

    Add logistic regression

--- scripts/04-model.R
+++ scripts/04-model.R
@@ -1,37 +1,41 @@
 #### Preamble ####
-# Purpose: Models... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Models whether we guessed right, based on our confidence
+# Author: Rahma Binth Mohammad
+# Date: 26 March 2024
+# Contact: rahma.binthmohammad@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
-
+# Pre-requisites: 02-data_cleaning.R
+# Any other information needed? -
 
 #### Workspace setup ####
 library(tidyverse)
 library(rstanarm)
+library(arrow)
 
 #### Read data ####
-analysis_data <- read_csv("data/analysis_data/analysis_data.csv")
+analysis_data <- read_parquet(file = "data/analysis_data/analysis_data.parquet")
+
+analysis_data |>
+  ggplot(aes(x = certainty, y = outcome)) +
+  geom_jitter(height = 0)
+
 
 ### Model data ####
 first_model <-
   stan_glm(
-    formula = flying_time ~ length + width,
+    formula = outcome ~ certainty,
     data = analysis_data,
-    family = gaussian(),
+    family = binomial(link = "logit"),
     prior = normal(location = 0, scale = 2.5, autoscale = TRUE),
     prior_intercept = normal(location = 0, scale = 2.5, autoscale = TRUE),
     prior_aux = exponential(rate = 1, autoscale = TRUE),
     seed = 853
   )
 
+summary(first_model)
 
 #### Save model ####
 saveRDS(
   first_model,
   file = "models/first_model.rds"
 )
-
-
